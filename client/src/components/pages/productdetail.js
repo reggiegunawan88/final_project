0590d5d465c8f7f1f140bbd2b9822616da42cd23
Reg <reggiegunawan88@gmail.com>
2020-11-25 22:02:30 +0700

    Fix css on product detail page

--- client/src/components/pages/productdetail.js
+++ client/src/components/pages/productdetail.js
@@ -2,19 +2,29 @@ import React from "react";
 import "./../style/productdetail.css";
 import ImageSlideshow from "../page_components/productdetail/img_slideshow";
 import ProductData from "../page_components/productdetail/productdata";
+import { Button } from "react-bootstrap";
 // import { observable } from "mobx";
 
 const productdetail = (props) => {
   const data = props.location.data;
 
+  function prev_page() {
+    props.history.goBack();
+  }
+
   return (
-    <div className="container">
-      <div className="row">
+    <div className="container container-product">
+      <div className="row mt-100">
         <div className="col-sm-12 mt-5">
           <div className="product-title">
             {data.merk} {data.nama}
           </div>
         </div>
+        {/* <div>
+          <Button onClick={prev_page}>
+            <b>KEMBALI</b>
+          </Button>
+        </div> */}
       </div>
       <div className="row mt-5 mb-5">
         <div className="col-sm-9">
